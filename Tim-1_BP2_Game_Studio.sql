ae4355950fdbf6512b8a95204fc32ad5ecda404b
beboid <juraj.stern.vukotic@gmail.com>
2024-01-15 15:56:35 +0100

    add vales for dice, size, alignment, creature_type, challenge_rating

--- Tim-1_BP2_Game_Studio.sql
+++ Tim-1_BP2_Game_Studio.sql
@@ -22,26 +22,26 @@ CREATE TABLE skill (
 
 CREATE TABLE dice(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	dice ENUM ("d4", "d6", "d8", "d10", "d12", "d20") UNIQUE NOT NULL
+	dice ENUM ('d4', 'd6', 'd8', 'd10', 'd12', 'd20') UNIQUE NOT NULL
 );
-
+    
 CREATE TABLE size(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	size ENUM ("TINY", "SMALL", "MEDIUM", "LARGE", "HUGE", "GARGANTUAN") NOT NULL,
+	size ENUM ('TINY', 'SMALL', 'MEDIUM', 'LARGE', 'HUGE', 'GARGANTUAN') NOT NULL,
 	space INT NOT NULL,
     UNIQUE (size, space)
 );
-
+    
 CREATE TABLE alignment(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	lawfulness ENUM ("LAWFUL", "NEUTRAL", "CHAOTIC") NOT NULL,
 	morality ENUM ("GOOD", "NEUTRAL", "EVIL") NOT NULL,
     UNIQUE (lawfulness, morality)
 );
-
+    
 CREATE TABLE creature_type(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	creature_type ENUM ("ABERRATION", "BEAST", "CELESTIAL", "CONSTRUCT", "DRAGON", "ELEMENTAL", "FEY", "FIEND", "GIANT", "HUMANOID", "MONSTROSITY", "OOZE", "PLANT", "UNDEAD") UNIQUE NOT NULL DEFAULT "HUMANOID"
+	creature_type ENUM ('ABERRATION', 'BEAST', 'CELESTIAL', 'CONSTRUCT', 'DRAGON', 'ELEMENTAL', 'FEY', 'FIEND', 'GIANT', 'HUMANOID', 'MONSTROSITY', 'OOZE', 'PLANT', 'UNDEAD') UNIQUE NOT NULL DEFAULT "HUMANOID"
 );
 
 CREATE TABLE challenge_rating (
@@ -49,7 +49,7 @@ CREATE TABLE challenge_rating (
     experience_points INT NOT NULL DEFAULT 0, 
     CHECK (experience_points >= 0)
 );
-
+    
 CREATE TABLE creature_template (
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	creature_name VARCHAR (64) UNIQUE NOT NULL,
@@ -709,6 +709,25 @@ INSERT INTO skill (skill_name, ability_score_id) VALUES
     ('Animal Handling', 5), ('Insight', 5), ('Medicine', 5), ('Perception', 5), ('Survival', 5), 
     ('Deception', 6), ('Intimidation', 6), ('Performance', 6), ('Persuasion', 6);
 
+INSERT INTO dice (dice) VALUES
+	('d4'), ('d6'), ('d8'), ('d10'), ('d12'), ('d20');
+    
+INSERT INTO size (size, space) VALUES
+	('TINY', 2), ('SMALL', 3), ('MEDIUM', 5), ('LARGE', 10), ('HUGE', 15), ('GARGANTUAN', 20);
+
+INSERT INTO alignment (lawfulness, morality) VALUES
+	('LAWFUL', 'GOOD'), ('LAWFUL', 'NEUTRAL'), ('LAWFUL', 'EVIL'), 
+    ('NEUTRAL', 'GOOD'), ('NEUTRAL', 'NEUTRAL'), ('NEUTRAL', 'EVIL'), 
+    ('CHAOTIC', 'GOOD'), ('CHAOTIC', 'NEUTRAL'), ('CHAOTIC', 'EVIL');
+    
+INSERT INTO creature_type (creature_type) VALUES
+	('ABERRATION'), ('BEAST'), ('CELESTIAL'), ('CONSTRUCT'), ('DRAGON'), ('ELEMENTAL'), ('FEY'), ('FIEND'), ('GIANT'), ('HUMANOID'), ('MONSTROSITY'), ('OOZE'), ('PLANT'), ('UNDEAD');
+    
+INSERT INTO challenge_rating (rating, experience_points) VALUES
+	(0, 10), (0.125, 25), (0.25, 50), (0.5, 100), (1, 200), (2, 450), (3, 700), (4, 1100), (5, 1800), 
+    (6, 2300), (7, 2900), (8, 3900), (9, 5000), (10, 5900), (11, 7200), (12, 8400), (13, 10000), (14, 11500), (15, 13000), 
+    (16, 15000), (17, 18000), (18, 20000), (19, 22000), (20, 25000), (21, 33000), (22, 41000), (23, 50000), (24, 62000), (25, 75000), 
+    (26, 90000), (27, 105000), (28, 120000), (29, 135000), (30, 115000);
 
 #------------------------------------
 # VIEWS
