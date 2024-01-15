464a93578d8df169b2aaa5cc3230404b9dcc7430
JurajSV <j.stern.vukotic@gmail.com>
2024-01-15 19:48:41 +0100

    add experience_for_level and comments with missing tables

--- Tim-1_BP2_Game_Studio_Values.sql
+++ Tim-1_BP2_Game_Studio_Values.sql
@@ -46,12 +46,73 @@ INSERT INTO languages (language_name) VALUES
 INSERT INTO languages (language_name, is_exotic) VALUES
 ('Abyssal', true), ('Celestial', true), ('Draconic', true), ('Deep Speech', true), ('Infernal', true), ('Primordial', true), ('Sylvan', true), ('Undercommon', true);
 
+INSERT INTO experience_for_level (which_level, experience_needed) VALUES
+(1, 0), (2, 300), (3, 900), (4, 2700), (5, 6500), (6, 14000), (7, 23000), (8, 34000), (9, 48000), (10, 64000), (11, 85000), (12, 100000),
+(13, 120000), (14, 140000), (15, 165000), (16, 195000), (17, 225000), (18, 265000), (19, 305000), (20, 355000);
+
 # CREATURE TEMPLATE
-
 # SKILL PROFICIENCY
-    
 # CONDITION RELATIONSHIP KAKO TREBA 
-
 # creature_condition_relationship kako treba
-
+# sense as needed
+# creature_sense as needed
+# movement as needed
+# item as needed 
+# creature instance as needed
+# creature item as needed
+# armor as needed
+# weapon as needed
+# weapon properties as needed
+# spell as needed
+# components as needed
+# spell_components as needed
+# aoe_shape as needed
+# spell_aoe_shape as needed 
+# class ass needed
+# spell_class as needed 
+# race as needed
+# weapon_property_match as needed 
+# features as needed
+# race_feature as needed 
+# race_item_prof as needed
+# racial_spells as needed 
+# race_skill_prof as needed
+# race_damage_relationship as needed 
+# race_movement as needed 
+# race_sense as needed
+# race_asi as needed
+# common_names as needed
+# race_names as needed
+# race_language as needed
+# race_condition_relationship as needed 
+# personality as needed
+# player as needed
+# background as needed
+# background_skills as needed
+# background_item_prof as needed 
+# background_languages as needed
+# background_equipment as needed
+# light_source as needed 
+# consumable as needed
+# creature_condition as needed 
+# creature_instance_spell_slots as needed 
+# creature_template_spells_known
+# creature_instance_spells_known
+# creature_instance_inventory 
+# game_instance 
+# game_players
+# map
+# map_creatures
+# class_saving_prof
+# player_character
+# notes
+# object_template
+# object_damage_relationship
+# object_instance
+# class_proficiency
+# class_levels 
+# class_level_feature
+# class_level_spellslots
+# throwable
+# 
 
