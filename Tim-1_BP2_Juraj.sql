bd2a8d8baa4b1fd4c84f1a6c2cab146fbf579b6c
JurajSV <j.stern.vukotic@gmail.com>
2024-01-16 22:46:48 +0100

    add more fields to statblock

--- Tim-1_BP2_Juraj.sql
+++ Tim-1_BP2_Juraj.sql
@@ -29,21 +29,29 @@ JOIN ability_score a ON s.ability_score_id = a.id;
 DROP VIEW IF EXISTS stat_block_template;
 CREATE VIEW stat_block_template AS
 SELECT 
-	crea.creature_name, s.size, t.creature_type, CONCAT(a.lawfulness, ' ', a.morality) AS alignment, crea.hit_dice_number, d.dice, 
+	crea.creature_name, s.size, t.creature_type, CONCAT(a.lawfulness, ' ', a.morality) AS alignment, CONCAT(crea.hit_dice_number, d.dice) AS hit_dice, 
+    CONCAT(GROUP_CONCAT( DISTINCT CONCAT(mov.movement, ' ' ,mov.distance)ORDER BY mov.movement SEPARATOR 'ft. '), 'ft. ') AS movement,
     CONCAT(crea.STRENGTH, ' (', calc_mod(crea.STRENGTH), ')') AS STR,
     CONCAT(crea.DEXTERITY, ' (', calc_mod(crea.DEXTERITY), ')') AS DEX,
     CONCAT(crea.CONSTITUTION, ' (', calc_mod(crea.CONSTITUTION), ')') AS CON,
     CONCAT(crea.INTELLIGENCE, ' (', calc_mod(crea.INTELLIGENCE), ')') AS INTE,
     CONCAT(crea.WISDOM, ' (', calc_mod(crea.WISDOM), ')') AS WIS,
     CONCAT(crea.CHARISMA, ' (', calc_mod(crea.CHARISMA), ')') AS CHR,
-	GROUP_CONCAT(lang.language_name ORDER BY lang.language_name SEPARATOR ', ') AS languages_spoken
+	GROUP_CONCAT(lang.language_name ORDER BY lang.language_name SEPARATOR ', ') AS languages,
+    GROUP_CONCAT(DISTINCT CONCAT(sen.sense, ' ', sen.distance, 'ft') ORDER BY sen.sense SEPARATOR ', ') AS senses,
+    crea.challenge_rating, cr.experience_points
 FROM creature_template AS crea
 JOIN size AS s ON crea.size_id = s.id
 JOIN creature_type AS t ON crea.creature_type_id = t.id
 JOIN alignment AS a ON crea.alignment_id = a.id
 JOIN dice AS d ON crea.hit_dice_type_id = d.id
+JOIN challenge_rating AS cr ON crea.challenge_rating = cr.rating
 LEFT JOIN creature_language AS cl ON crea.id = cl.creature_id
 LEFT JOIN languages AS lang ON cl.language_id = lang.id
+LEFT JOIN creature_sense AS cs ON crea.id = cs.creature_id
+LEFT JOIN sense AS sen ON cs.sense_id = sen.id
+LEFT JOIN creature_movement AS cmov ON crea.id = cmov.creature_id
+LEFT JOIN movement AS mov ON cmov.movement_id = mov.id
 GROUP BY crea.creature_name, s.size, t.creature_type, a.lawfulness, a.morality, crea.hit_dice_number, d.dice, crea.STRENGTH, crea.DEXTERITY, crea.CONSTITUTION, crea.INTELLIGENCE, crea.WISDOM, crea.CHARISMA;
  
 
