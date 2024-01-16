7e990a028ac68c3f059704f2a96964212e18b9eb
JurajSV <j.stern.vukotic@gmail.com>
2024-01-16 18:32:10 +0100

    formatting

--- Tim-1_BP2_Game_Studio_Values.sql
+++ Tim-1_BP2_Game_Studio_Values.sql
@@ -50,29 +50,29 @@ INSERT INTO experience_for_level (which_level, experience_needed) VALUES
 
 # conditions
 INSERT INTO conditions (condition_name, condition_description) VALUES
-('BLINDED', 'A blinded creature can’t see and automatically fails any ability check that requires sight. Attack rolls against the creature have advantage, and the creature’s attack rolls have disadvantage.'),
-('CHARMED', 'A charmed creature can’t attack the charmer or target the charmer with harmful abilities or magical effects. The charmer has advantage on any ability check to interact socially with the creature.'),
-('DEAFENED', 'A deafened creature can’t hear and automatically fails any ability check that requires hearing.'),
-('FRIGHTENED', 'A frightened creature has disadvantage on ability checks and attack rolls while the source of its fear is within line of sight. The creature can’t willingly move closer to the source of its fear.'),
-('GRAPPLED', 'A grappled creature’s speed becomes 0, and it can’t benefit from any bonus to its speed. The condition ends if the grappler is incapacitated (see the condition). The condition also ends if an effect removes the grappled creature from the reach of the grappler or grappling effect, such as when a creature is hurled away by the thunderwave spell.'),
-('INCAPACITATED', 'An incapacitated creature can’t take actions or reactions.'),
-('INVISIBLE', 'An invisible creature is impossible to see without the aid of magic or a special sense. For the purpose of hiding, the creature is heavily obscured. The creature’s location can be detected by any noise it makes or any tracks it leaves. Attack rolls against the creature have disadvantage, and the creature’s attack rolls have advantage.'),
-('PARALYZED', 'A paralyzed creature is incapacitated (see the condition) and can’t move or speak. The creature automatically fails Strength and Dexterity saving throws. Attack rolls against the creature have advantage. Any attack that hits the creature is a critical hit if the attacker is within 5 feet of the creature.'),
-('PETRIFIED', 'A petrified creature is transformed, along with any nonmagical object it is wearing or carrying, into a solid inanimate substance (usually stone). Its weight increases by a factor of ten, and it ceases aging. The creature is incapacitated (see the condition), can’t move or speak, and is unaware of its surroundings. Attack rolls against the creature have advantage. The creature automatically fails Strength and Dexterity saving throws. The creature has resistance to all damage. The creature is immune to poison and disease, although a poison or disease already in its system is suspended, not neutralized.'),
-('POISONED', 'A poisoned creature has disadvantage on attack rolls and ability checks.'),
-('PRONE', 'A prone creature’s only movement option is to crawl, unless it stands up and thereby ends the condition. The creature has disadvantage on attack rolls. An attack roll against the creature has advantage if the attacker is within 5 feet of the creature. Otherwise, the attack roll has disadvantage.'),
-('RESTRAINED', 'A restrained creature’s speed becomes 0, and it can’t benefit from any bonus to its speed. Attack rolls against the creature have advantage, and the creature’s attack rolls have disadvantage. The creature has disadvantage on Dexterity saving throws.'),
-('STUNNED', 'A stunned creature is incapacitated (see the condition), can’t move, and can speak only falteringly. The creature automatically fails Strength and Dexterity saving throws. Attack rolls against the creature have advantage.'),
-('UNCONSCIOUS', 'An unconscious creature is incapacitated (see the condition), can’t move or speak, and is unaware of its surroundings The creature drops whatever it’s holding and falls prone. The creature automatically fails Strength and Dexterity saving throws. Attack rolls against the creature have advantage. Any attack that hits the creature is a critical hit if the attacker is within 5 feet of the creature.'),
-('EXHAUSTION 1', 'Disadvantage on ability checks'),
-('EXHAUSTION 2', 'Speed halved'),
-('EXHAUSTION 3', 'Disadvantage on attack rolls and saving throws'),
-('EXHAUSTION 4', 'Hit point maximum halved'),
-('EXHAUSTION 5', 'Speed reduced to 0'),
-('EXHAUSTION 6', 'Death');
+	('BLINDED', 'A blinded creature can’t see and automatically fails any ability check that requires sight. Attack rolls against the creature have advantage, and the creature’s attack rolls have disadvantage.'),
+	('CHARMED', 'A charmed creature can’t attack the charmer or target the charmer with harmful abilities or magical effects. The charmer has advantage on any ability check to interact socially with the creature.'),
+	('DEAFENED', 'A deafened creature can’t hear and automatically fails any ability check that requires hearing.'),
+	('FRIGHTENED', 'A frightened creature has disadvantage on ability checks and attack rolls while the source of its fear is within line of sight. The creature can’t willingly move closer to the source of its fear.'),
+	('GRAPPLED', 'A grappled creature’s speed becomes 0, and it can’t benefit from any bonus to its speed. The condition ends if the grappler is incapacitated (see the condition). The condition also ends if an effect removes the grappled creature from the reach of the grappler or grappling effect, such as when a creature is hurled away by the thunderwave spell.'),
+	('INCAPACITATED', 'An incapacitated creature can’t take actions or reactions.'),
+	('INVISIBLE', 'An invisible creature is impossible to see without the aid of magic or a special sense. For the purpose of hiding, the creature is heavily obscured. The creature’s location can be detected by any noise it makes or any tracks it leaves. Attack rolls against the creature have disadvantage, and the creature’s attack rolls have advantage.'),
+	('PARALYZED', 'A paralyzed creature is incapacitated (see the condition) and can’t move or speak. The creature automatically fails Strength and Dexterity saving throws. Attack rolls against the creature have advantage. Any attack that hits the creature is a critical hit if the attacker is within 5 feet of the creature.'),
+	('PETRIFIED', 'A petrified creature is transformed, along with any nonmagical object it is wearing or carrying, into a solid inanimate substance (usually stone). Its weight increases by a factor of ten, and it ceases aging. The creature is incapacitated (see the condition), can’t move or speak, and is unaware of its surroundings. Attack rolls against the creature have advantage. The creature automatically fails Strength and Dexterity saving throws. The creature has resistance to all damage. The creature is immune to poison and disease, although a poison or disease already in its system is suspended, not neutralized.'),
+	('POISONED', 'A poisoned creature has disadvantage on attack rolls and ability checks.'),
+	('PRONE', 'A prone creature’s only movement option is to crawl, unless it stands up and thereby ends the condition. The creature has disadvantage on attack rolls. An attack roll against the creature has advantage if the attacker is within 5 feet of the creature. Otherwise, the attack roll has disadvantage.'),
+	('RESTRAINED', 'A restrained creature’s speed becomes 0, and it can’t benefit from any bonus to its speed. Attack rolls against the creature have advantage, and the creature’s attack rolls have disadvantage. The creature has disadvantage on Dexterity saving throws.'),
+	('STUNNED', 'A stunned creature is incapacitated (see the condition), can’t move, and can speak only falteringly. The creature automatically fails Strength and Dexterity saving throws. Attack rolls against the creature have advantage.'),
+	('UNCONSCIOUS', 'An unconscious creature is incapacitated (see the condition), can’t move or speak, and is unaware of its surroundings The creature drops whatever it’s holding and falls prone. The creature automatically fails Strength and Dexterity saving throws. Attack rolls against the creature have advantage. Any attack that hits the creature is a critical hit if the attacker is within 5 feet of the creature.'),
+	('EXHAUSTION 1', 'Disadvantage on ability checks'),
+	('EXHAUSTION 2', 'Speed halved'),
+	('EXHAUSTION 3', 'Disadvantage on attack rolls and saving throws'),
+	('EXHAUSTION 4', 'Hit point maximum halved'),
+	('EXHAUSTION 5', 'Speed reduced to 0'),
+	('EXHAUSTION 6', 'Death');
 
     
-    SELECT * FROM skill;
+SELECT * FROM skill;
 
 # CREATURE TEMPLATE # ('', size, type, align, str, dex, con, int, wis, chr, prof, hitdice, hitdicenum, cr)
 INSERT INTO creature_template (creature_name, size_id, creature_type_id, alignment_id, STRENGTH, DEXTERITY, CONSTITUTION, INTELLIGENCE, WISDOM, CHARISMA, proficiency, hit_dice_type_id, hit_dice_number, challenge_rating) VALUES
